2f3e390645f761aa480e646713fc98515948aa73
darinatsybenko <110915599+darinatsybenko@users.noreply.github.com>
2022-12-19 20:18:05 +0300

    js
    add modal

---
+++ js/01-gallery.js
@@ -0,0 +1,53 @@
+import { galleryItems } from './gallery-items.js';
+console.log(galleryItems); 
+
+ const galleryContainer = document.querySelector('.gallery')
+ console.log(galleryContainer);
+ const murkUp = createGalleryItem (galleryItems);
+ console.log(murkUp);
+ galleryContainer.insertAdjacentHTML('beforeend', murkUp)
+const galleryEl = document.querySelector('.gallery');
+console.log(galleryEl);
+galleryEl.addEventListener('click', onClick)
+
+
+function createGalleryItem (galleryItems) {
+return galleryItems.map(({preview, original, description}) => {
+    return `
+    <div class="gallery__item">
+  <a class="gallery__link" href="${original}">
+    <img
+      class="gallery__image"
+      src="${preview}"
+      data-source="${original}"
+      alt="${description}"
+    />
+  </a>
+</div>
+    `;
+})
+.join('');
+};
+console.log(createGalleryItem);
+
+function onClick(evt) {
+    evt.preventDefault();
+const urlImg = evt.target.getAttribute("data-source")
+
+    const instance = basicLightbox.create(`
+   
+    <img
+    
+      src="${urlImg}"
+      width="100"
+      height="100"
+    />
+   
+`);
+instance.show();
+};
+
+
+
+
+ 
